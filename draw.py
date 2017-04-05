15bbabbadde8630bb54d4654794108ace451ae2d
getDolla <ywang8@stuy.edu>
2017-04-04 21:38:18 -0400

    finished add sphere

--- draw.py
+++ draw.py
@@ -60,12 +60,12 @@ def add_box( points, x, y, z, width, height, depth ):
     #back
     add_polygon(points, x1, y, z1, x1, y1, z1, x, y1, z1)
     add_polygon(points, x, y1, z1, x, y, z1, x1, y, z1)
-    
+
 def add_sphere( edges, cx, cy, cz, r, step ):
     l = generate_sphere( cx, cy, cz, r, step )
     num_steps = int(1/step+0.1)
     i = 0
-    
+
     while i < len(l):
         x0 = l[i][0]
         y0 = l[i][1]
@@ -73,13 +73,18 @@ def add_sphere( edges, cx, cy, cz, r, step ):
 
         x1 = l[i + 1][0]
         y1 = l[i + 1][1]
-        z1 = l[i = 1][2]
+        z1 = l[i + 1][2]
 
-        x2 = l[i][0]
-        y2 = l[i][1]
-        z2 = l[i][2]
+        x2 = l[i + num_steps + 1][0]
+        y2 = l[i + num_steps + 1][1]
+        z2 = l[i + num_steps + 1][2]
 
-        add_edge( points, x0, y0, z0, x0, y0, z0 )
+        x3 = l[i + num_steps][0]
+        y3 = l[i + num_steps][1]
+        z3 = l[i + num_steps][2]
+
+        add_polygon( points, x0, y0, z0, x1, y1, z1, x2, y2, z2 )
+        add_polygon( points, x2, y2, z2, x3, y3, z3, x0, y0, z0 )
         i += 1
 
 def generate_sphere( cx, cy, cz, r, step ):
